836a47f4f8e5ae453574813e89b5bfdc4a938f0f
刘攀 <lp123092@163.com>
2023-12-14 22:49:43 +0800

    优化

--- ws-facade/src/main/java/com/lp/service/PushService.java
+++ ws-facade/src/main/java/com/lp/service/PushService.java
@@ -73,7 +73,7 @@ public class PushService {
                 }
                 serviceName = LocalCache.wsUser.get(id + ":" + request.getDeviceType());
             }
-            if (StrUtil.isBlank(serviceName)) {
+            if (StrUtil.isNotBlank(serviceName)) {
                 serviceNameList.add(serviceName);
             }
         }
